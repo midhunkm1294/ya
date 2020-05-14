290e0db0770e64b87b98dd344bf61da39ef379d3
Indian Bhai <61813271+indianbhaiya@users.noreply.github.com>
2020-05-14 14:47:13 +0530

    Update config.py

--- haruka/config.py
+++ haruka/config.py
@@ -20,7 +20,7 @@ class Config(object):
     LOAD = []
     NO_LOAD = ['translation', 'sed']
     WEBHOOK = ANYTHING
-    URL = "https://itsflashbot.herokuapp.com"
+    URL = "https://killerbotapp.herokuapp.com"
 
     # OPTIONAL
     SUDO_USERS = []  # List of id's (not usernames) for users which have sudo access to the bot.
